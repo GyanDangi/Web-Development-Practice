81abb9ac6471b7f4f9a485ad768f42628282f817
Gyanprakash <gyanprakashdangi@gmail.com>
2023-03-04 22:24:46 +0530

    JavaScript Basic

---
+++ JS_Basic_I/script.js
@@ -0,0 +1,24 @@
+console.log("helooo");
+let a=true;
+console.log(a);
+// let a=5; we can't redeclare it
+
+var b=45;
+console.log(b);
+var b=90;
+console.log(b);
+
+let num=898;
+console.log(num);
+num="Gyan";
+console.log(num);
+
+//Equality Operator:
+// : Loose equality: == only value is checked
+// : strict equality: === value + data both are chcked
+
+// Ternary Operator:
+let age=34;
+let ageStatus= (age>=18)? "Can vote":"can't Vote";
+
+console.log(ageStatus);
